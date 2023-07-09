4fe60c3fe4a3996473fb37c123d9752bbaa387b1
xxAROX <arox.email.0@gmail.com>
2023-07-09 23:33:49 +0200

    base

---
+++ src/xxAROX/PresenceMan/Connection.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+namespace xxAROX\PresenceMan;
+use pocketmine\thread\Thread;
+
+
+/**
+ * Class Connection
+ * @package xxAROX\PresenceMan
+ * @author Jan Sohn / xxAROX
+ * @date 09. Juli, 2023 - 22:41
+ * @ide PhpStorm
+ * @project pmmp
+ */
+class Connection extends Thread{
+	protected function onRun(): void{
+		// TODO: Implement onRun() method.
+	}
+}
--- src/xxAROX/PresenceMan/PresenceMan.php
+++ src/xxAROX/PresenceMan/PresenceMan.php
@@ -1,19 +1,27 @@
 <?php
-
+namespace xxAROX\PresenceMan;
 use pocketmine\plugin\PluginBase;
 
 
+/**
+ * Class PresenceMan
+ * @package xxAROX\PresenceMan
+ * @author Jan Sohn / xxAROX
+ * @date 09. Juli, 2023 - 22:30
+ * @ide PhpStorm
+ * @project pmmp
+ */
 final class PresenceMan extends PluginBase {
     public static string $NETWORK = "undefined";
-    public static string $SERVER_NAME = "undefined";
+    public static string $SERVER = "undefined";
 
 
     public function onLoad(): void{
-        $this->saveResources("config.yml");
+        $this->saveResource("config.yml");
         $config = $this->getConfig();
-        self::$NETWORK = getenv("PRESENCE_MAN_NETWORK") === false || empty(getenv("PRESENCE_MAN_NETWORK")) ? $config->get("network", self::$NETWORK) : getenv("PRESENCE_MAN_NETWORK");
-        self::$SERVER_NAME = getenv("PRESENCE_MAN_SERVER_NAME") === false || empty(getenv("PRESENCE_MAN_SERVER_NAME")) ? $config->get("server-name", self::$SERVER_NAME) : getenv("PRESENCE_MAN_SERVER_NAME");
-        $this->getLogger()->notice("This server is " . self::$SERVER_NAME . " on " . self::$NETWORK . "!");
+        self::$NETWORK = getenv("PRESENCE_MAN_NETWORK") == false || empty(getenv("PRESENCE_MAN_NETWORK")) ? $config->get("network", self::$NETWORK) : getenv("PRESENCE_MAN_NETWORK");
+        self::$SERVER = getenv("PRESENCE_MAN_SERVER") == false || empty(getenv("PRESENCE_MAN_SERVER")) ? $config->get("server", self::$SERVER) : getenv("PRESENCE_MAN_SERVER");
+        $this->getLogger()->notice("This server will be displayed as " . self::$SERVER . " on " . self::$NETWORK . " in rich-presences!");
     }
 
     public function onEnable(): void{
